93819069232bfd3b1d023059042eafc90ee902d3
Manpreet Mann <manpreetmann4788@gmail.com>
2024-06-14 20:15:05 +0530

    updated resume

--- src/utils/data/contactsData.js
+++ src/utils/data/contactsData.js
@@ -7,6 +7,6 @@ export const contactsData = {
     linkedIn: 'https://www.linkedin.com/in/manpreet-mann/',
     leetcode:'',
     twitter:'https://x.com/manpreetmann33',
-    resume: 'https://drive.google.com/file/d/1rwcU6D2GfTUOj9oydQDrzaCbW77JkW8Z/view',
+    resume: 'https://drive.google.com/file/d/1wTierfeegCYrdSxVWmzafuougv-JMSxn/view?usp=sharing',
     aboutMe:'I am a recent B.Tech graduate specializing in Computer Science & Engineering from I.K. Gujral Punjab Technical University, Kapurthala. My skill set includes proficiency in React, HTML5, CSS3, JavaScript, Bootstrap, and Figma. Additionally, I am expanding my expertise in backend development using Node.js, Express, and MongoDB. I also possess a strong foundation in C++.'
 }
